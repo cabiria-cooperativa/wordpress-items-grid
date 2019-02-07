8ca28099c89040b67818cd0850e251c3b4847f9c
Simone Alati <git@simonealati.it>
2019-02-07 09:08:39 +0100

    no message

--- cabi-items-grid.php
+++ cabi-items-grid.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.cabiria.net
  * Description: Plugin che mostra una griglia di 12 item (in 4 colonne) con aggiornamento schedulato. Aggiunge un custom post type "item-grid". Alla disattivazione rimuove tutti i custom creati. Si utilizza tramite shortcode [cabi_items_grid]. E' possibile decidere il numero di righe e di "vuoti" passando dei parametri allo shortcode. Ad es. [cabi_items_grid rows=4 hollows=5]
  * Version: 1.0.0
- * Author: Cabiria
+ * Author: Simone Alati
  * Author URI: https://www.cabiria.net
  * Text Domain: cabi
  */
